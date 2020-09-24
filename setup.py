f0b4cd231c6ea089d3f4ceddfe40768584b7385f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-09-24 13:56:24 +0300

    Added Manifest.in file.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["Util/*.pyx", "Util/*.pxd"], compiler_directives={'language_level' : "3"}),
     name='NlpToolkit-Util-Cy',
-    version='1.0.0',
+    version='1.0.1',
     packages=['Util'],
     url='https://github.com/olcaytaner/Util-Cy',
     license='',
