624a432bb87eb6e9d32f5aa4bf6fad908a48ad31
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-09-26 11:24:58 +0300

    Added package_data option.

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["Util/*.pyx", "Util/*.pxd"], compiler_directives={'language_level' : "3"}),
     name='NlpToolkit-Util-Cy',
-    version='1.0.1',
+    version='1.0.2',
     packages=['Util'],
+    package_data={'Util': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/olcaytaner/Util-Cy',
     license='',
     author='olcaytaner',
